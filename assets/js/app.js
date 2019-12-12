fc6e9caa4ee0b369fea1cc14c6951b6b2bd2b4b9
seffenN <narjes.seffen@esprit.tn>
2019-12-13 00:56:31 +0100

    2.1

---
+++ assets/js/app.js
@@ -0,0 +1,24 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you require will output into a single css file (app.css in this case)
+require('../css/app.css');
+require('../css/catalog/view/javascript/magnific-popup.css');
+require('../css/catalog/view/theme/Stable/stylesheet/codezeel/bootstrap.min.css');
+require('../css/catalog/view/theme/Stable/stylesheet/codezeel/animate.css');
+require('../css/catalog/view/theme/Stable/stylesheet/codezeel/carousel.css');
+require('../css/catalog/view/theme/Stable/stylesheet/codezeel/custom.css');
+require('../css/catalog/view/theme/Stable/stylesheet/codezeel/lightbox.css');
+require('../css/catalog/view/theme/Stable/stylesheet/stylesheet.css');
+require('../css/catalog/view/javascript/swiper/css/opencart.css');
+require('../css/catalog/view/javascript/swiper/css/swiper.min.css');
+require('../css/catalog/view/javascript/bootstrap-datetimepicker.min.css');
+
+// Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
+// const $ = require('jquery');
+
+console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
